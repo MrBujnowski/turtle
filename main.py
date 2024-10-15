cd0344cc8140457056dab0cf9acc272db143293e
Antonín Bujnovský <120708132+MrBujnowski@users.noreply.github.com>
2024-10-15 07:10:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import *
+import random
+import turtle
 from turtle import *
 
 shape("turtle")
@@ -19,6 +20,7 @@ def slunicko():
                 forward(20)
 
 def sachovnice():
+        x=0
 
         speed(4000)
         penup()
@@ -27,28 +29,73 @@ def sachovnice():
         forward(300)
         left(90)
         pendown()
-        for k in range(8):
-                for i in range(8):
-                        for j in range(4):
+        for i in range(8):
+                if i == 2 or i == 4 or i == 6:
+                        penup()
+                        backward(50)
+                        pendown()
+                for j in range(8):
+                        if (j+x) % 2 == 0:
+                                penup()
                                 forward(50)
-                                right(90)
-                        forward(50)
+                                pendown()
+                                for k in range(25):
+                                        right(90)
+                                        forward(50)
+                                        left(90)
+                                        forward(1)
+                                        left(90)
+                                        forward(50)
+                                        right(90)
+                                        forward(1)
+                        if (j+x) % 2 != 0:
+                                for l in range(4):
+                                        forward(50)
+                                        right(90)
+                                        forward(50)
+                                        right(90)
+                penup()
                 backward(400)
                 left(90)
-                penup()
                 forward(50)
                 pendown()
                 right(90)
+                if x % 2 == 0:
+                        penup()
+                        forward(50)
+                        pendown()
+                x+=1
 
 
+def divny_tvar():
 
-def divnyTvar():
-
-        a=100
         pencolor("blue")
-        forward(a)
+        speed(1000)
+        a = [0,90,180,270]
+        while True:
+                forward(30)
+                right(random.choice(a))
 
+def strom():
+        a=250
+        b = [90,270]
+        speed(1000)
+        left(90)
+        penup()
+        turtle.goto(0,-400)
+        pendown()
+        while True:
+                for i in range(10):
+                        forward(a)
+                        c = random.choice(b)
+                        left(c)
+                        forward(a)
+                        right(c)
+                        a=a/2
+                penup()
+                turtle.goto(0,-400)
+                pendown()
+                a=250
 
 
 mainloop()
-
